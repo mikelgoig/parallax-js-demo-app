7c26eb201d29c467a721d4a1291460d558700206
Mikel Goig <mikel@goig.org>
2018-11-02 14:59:57 +0100

    Compress images

--- src/components/App.js
+++ src/components/App.js
@@ -5,31 +5,10 @@ import styled from 'styled-components';
 /* Components */
 import Scene from './Scene';
 
-/* Scene 01 */
-import layer0101 from '../images/scene-01/layer-01.png';
-import layer0102 from '../images/scene-01/layer-02.png';
-import layer0103 from '../images/scene-01/layer-03.png';
-import layer0104 from '../images/scene-01/layer-04.png';
-import layer0105 from '../images/scene-01/layer-05.png';
-
-/* Scene 02 */
-import layer0201 from '../images/scene-02/layer-01.png';
-import layer0202 from '../images/scene-02/layer-02.png';
-import layer0203 from '../images/scene-02/layer-03.png';
-import layer0204 from '../images/scene-02/layer-04.png';
-import layer0205 from '../images/scene-02/layer-05.png';
-import layer0206 from '../images/scene-02/layer-06.png';
-import layer0207 from '../images/scene-02/layer-07.png';
-
-/* Scene 03 */
-import layer0301 from '../images/scene-03/layer-01.png';
-import layer0302 from '../images/scene-03/layer-02.png';
-import layer0303 from '../images/scene-03/layer-03.png';
-import layer0304 from '../images/scene-03/layer-04.png';
-import layer0305 from '../images/scene-03/layer-05.png';
-import layer0306 from '../images/scene-03/layer-06.png';
-import layer0307 from '../images/scene-03/layer-07.png';
-import layer0308 from '../images/scene-03/layer-08.png';
+/* Images */
+const scene01 = (name) => require.context('../images/scene-01', true)(name, true);
+const scene02 = (name) => require.context('../images/scene-02', true)(name, true);
+const scene03 = (name) => require.context('../images/scene-03', true)(name, true);
 
 /* Styled Components */
 const ButtonsWrapper = styled.div`
@@ -103,11 +82,11 @@ class App extends Component {
               isEnabledDeviceMotion={this.state.isEnabledDeviceMotion}
               isEnabledDeviceOrientation={this.state.isEnabledDeviceOrientation}
               layers={[
-                layer0101,
-                layer0102,
-                layer0103,
-                layer0104,
-                layer0105,
+                scene01('./layer-01.png'),
+                scene01('./layer-02.png'),
+                scene01('./layer-03.png'),
+                scene01('./layer-04.png'),
+                scene01('./layer-05.png'),
               ]}
             />
           </div>
@@ -117,13 +96,13 @@ class App extends Component {
               isEnabledDeviceMotion={this.state.isEnabledDeviceMotion}
               isEnabledDeviceOrientation={this.state.isEnabledDeviceOrientation}
               layers={[
-                layer0201,
-                layer0202,
-                layer0203,
-                layer0204,
-                layer0205,
-                layer0206,
-                layer0207,
+                scene02('./layer-01.png'),
+                scene02('./layer-02.png'),
+                scene02('./layer-03.png'),
+                scene02('./layer-04.png'),
+                scene02('./layer-05.png'),
+                scene02('./layer-06.png'),
+                scene02('./layer-07.png'),
               ]}
             />
           </div>
@@ -133,14 +112,14 @@ class App extends Component {
               isEnabledDeviceMotion={this.state.isEnabledDeviceMotion}
               isEnabledDeviceOrientation={this.state.isEnabledDeviceOrientation}
               layers={[
-                layer0301,
-                layer0302,
-                layer0303,
-                layer0304,
-                layer0305,
-                layer0306,
-                layer0307,
-                layer0308,
+                scene03('./layer-01.png'),
+                scene03('./layer-02.png'),
+                scene03('./layer-03.png'),
+                scene03('./layer-04.png'),
+                scene03('./layer-05.png'),
+                scene03('./layer-06.png'),
+                scene03('./layer-07.png'),
+                scene03('./layer-08.png'),
               ]}
             />
           </div>
